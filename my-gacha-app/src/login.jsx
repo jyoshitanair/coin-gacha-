93715ee1265ed472ad2ffd0a0afe9ef4e1e6e1ab
jyoshitanair <jyoshitanair123@gmail.com>
2026-06-24 11:12:34 -0700

    added the captcahs and signup page!

--- my-gacha-app/src/login.jsx
+++ my-gacha-app/src/login.jsx
@@ -2,26 +2,34 @@ import React from 'react';
 import { useState } from 'react';
 //supabase
 import { createClient } from '@supabase/supabase-js'
-
+import Signup from './signup.jsx'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+import { Turnstile } from '@marsidev/react-turnstile'
 
 //export allows it to be imported in other files
 //default means that it is the main (can only hv one) thingy of the file so no curly braces !
 
 export default function Login() {
+    const [captchaToken, setCaptchaToken] = useState()
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [page, setPage] = useState("login");
     async function handleLogin(e) {
         e.preventDefault();
+        if (!captchaToken) {
+            alert("uh oh. complete your captcha first!")
+            return;
+        }
         {/* this makes it so that the form doesnt refresh and lose all data
         only grab the value of error*/}
         const { error } = await supabase.auth.signInWithPassword(
             {
                 email: email,
-                password: password
+                password: password,
+                options: {captchaToken},
             }
         )
         if (error) {
@@ -32,6 +40,8 @@ export default function Login() {
     }
     return (
         <>
+            {page == "login" && 
+            <>
             <p> login page</p>
             <form onSubmit={handleLogin}>
                 {/* different types are text,email,number,checkbox,password*/}
@@ -39,18 +49,25 @@ export default function Login() {
                 no comments inside tags!*/}
                 <input
                     type="email"
-                    placeholder="..."
+                    placeholder="enter email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 <input
                     type="password"
-                    placeholder="..."
+                    placeholder="enter password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
                 <button type="submit" >Let's go!</button>
             </form>
+            <button type = "button" onClick ={() => setPage("signup")}>Sign up?</button>
+            <Turnstile
+                siteKey={import.meta.env.VITE_TURNSTILE_SITE_KEY}
+                onSuccess={(token) => {setCaptchaToken(token)}}
+            />
+            </>}
+            {page == "signup" && <Signup />}
         </>
     );
 }
---
+++ my-gacha-app/src/signup.jsx
@@ -0,0 +1,68 @@
+import react from 'react';
+import { useState } from 'react';
+import Login from './login.jsx'
+//supabase
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+import { Turnstile } from '@marsidev/react-turnstile'
+
+export default function Signup() {
+    const [page, setPage] = useState("signup");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleSignup(e){
+        e.preventDefault();
+        if (!captchaToken) {
+            alert("uh oh. complete your captcha first!")
+            return;
+        }
+        const {error} = await supabase.auth.signUp(
+            {
+                email: email,
+                password: password,
+                options: {captchaToken}
+            }
+        )
+        if (error){
+            alert(error.message)
+        }else{
+            alert("Account Created. Being redirected to login page")
+            setPage("login")
+        }
+    }
+
+
+    return(
+        <>
+        {page == "signup" && 
+        <>
+        <h1>Signup</h1>
+        <form>
+            <input
+            type = "email"
+            placeholder = "enter email"
+            value = {email}
+            onChange = {(e) => setEmail(e.target.value)}
+            />
+            <input
+            type = "password"
+            placeholder = "enter password"
+            value = {password}
+            onChange = {(e) => setPassword(e.target.value)}
+            />
+            <button type = "submit" onClick = {handleSignup}> Sign up </button>
+        </form>
+        <button type = "button" onClick = {( ) => setPage("login")}> Login </button>
+         <Turnstile
+                        siteKey={import.meta.env.VITE_TURNSTILE_SITE_KEY}
+                        onSuccess={(token) => {setCaptchaToken(token)}}
+        />
+        </>}
+
+        {page == "login" && <Login/>}
+        </>
+    );
+}
