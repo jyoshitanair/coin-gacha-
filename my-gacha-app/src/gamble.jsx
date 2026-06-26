5ba28e68f84299df953eb7745d931bae805c92bd
jyoshitanair <jyoshitanair123@gmail.com>
2026-06-26 00:40:38 -0700

    uh the formula works and rarity shows up :D need to make a log tho :/

--- my-gacha-app/src/gamble.jsx
+++ my-gacha-app/src/gamble.jsx
@@ -18,25 +18,26 @@ export default function Flashcards() {
         eyes: 1,
         mouths: 1,
         accessories: 1,
-        rarity: 1,
+        rarity: 0,
     });
     function percentCalc(points1,percent1,amount) {
         var final = amount
         var start = 1
-        if (percent1 <= 95){
-            points1 += 1
+        if (percent1 <= 92){
+            points1 += 3
             var max = Math.floor(amount/4)+1
             amount -= max
             final -= max
-        }if (percent1 <= 30){
-            points1 += 2
+        }if (percent1 <= 75){
+            points1 -= 2
             var max = Math.floor(amount/2)+1
             final -= max
-        }if(percent1 > 95){
-            points1 += 4
+        }if(percent1 > 92){
+            points1 += 5
         }
         // 1-5, 1-3,1
-        return Math.floor(Math.random() *(final-start+1))+start
+        var index = Math.floor(Math.random() *(final-start+1))+start
+        return [index,points1]
     }
     function createChar() {
         var points = 0
@@ -47,20 +48,21 @@ export default function Flashcards() {
             const percent = Math.random()*100.0
             //these have 9 !
             if((category == "body") || (category == "head") || (category == "hair")){
-                index = percentCalc(points,percent,9)
+                var calc = percentCalc(points,percent,9) 
             }
             //these have 11 !
             if((category == "accessories")){
-                index = percentCalc(points,percent,11)
+                var calc = percentCalc(points,percent,11) 
             }
             //these have 20 !
             if((category == "eyes") || (category == "mouths")){
-                index = percentCalc(points,percent,20)
+                var calc = percentCalc(points,percent,20) 
             }
-
+            index = calc[0]
+            points = calc[1]
             newChar[category] = index
        });
-       var rarity = (Math.floor((points - 18)/6)*5)+1
+       var rarity = Math.floor(((points - 6)*5)/18)+1
        if (rarity >= 5){
             rarity = 5
        }
@@ -69,12 +71,14 @@ export default function Flashcards() {
        }
        newChar["rarity"] = rarity
        setGacha(newChar)
-       console.log(newChar)
     };
     return(
     <>
         <p> gamble </p>
         <button onClick = {() => createChar()}> pull! </button>
+        {gacha.rarity != 0 &&
+            <p>Rarity: {gacha.rarity}</p>
+        }
     </>
     );
 }
