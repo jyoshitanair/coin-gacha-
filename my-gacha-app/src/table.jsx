bdd25820a73190730e6f6c48d69c6af2ad950dfd
jyoshitanair <jyoshitanair123@gmail.com>
2026-06-28 23:26:00 -0700

    bruh im so lost. trying to convert to a cutesy grid T-T

--- my-gacha-app/src/table.jsx
+++ my-gacha-app/src/table.jsx
@@ -11,6 +11,8 @@ export default function Table({uuid}) {
     const [loading, setLoading] = useState(true)
     const [pulls, setPulls] = useState(null)
     const [page, setPage] = useState("table")
+    const [pageNumber, setPageNumber] = useState(1)
+    const [disabled, setDisabled] = useState(false)
     useEffect(() => {
         async function get_pulls(){
         setLoading(true)
@@ -25,32 +27,54 @@ export default function Table({uuid}) {
         }
         get_pulls()
     }, []) 
+    //setting up page returning
+    const lastItemIndex = pageNumber *9;
+    const currentPulls = calculatePulls();
+    const totalPages = calculatePages();
+    useEffect(() => {
+        if(pageNumber == totalPages){
+            setDisabled(true);
+        }
+    },[pageNumber])
+    function calculatePages() {
+        if(pulls){
+            return Math.ceil((pulls.length)/9) //always need to round up a page
+        }else{
+            return 1; 
+        }
+    }
+    function calculatePulls() {
+        if(pulls){
+            //slices at a start and end
+            //pulls alr filtered to only show UR pulls
+            return pulls.slice(pageNumber,lastItemIndex)
+        }else{
+            return [];
+        }
+    }
+    
     return(
         <>
-            {page == "table" && 
+            {loading == true &&
                 <>
-                    <p> View all your pulls! </p>
-                    <button className = "buttons" onClick={() => setPage("gamble")}>gamble</button>
-                    {loading == true && <p> I'm loading!</p>}
-                    {loading == false && 
-                    <>
-                        <table>
-                            <thead>
-                                <th>ID</th>
-                                <th>Rarity</th>
-                                <th> Image</th>
-                            </thead>
-                            <tbody>
-                            {pulls && pulls.map((pull) => (
-                            <tr>
-                                <td> {pull.gacha_id}</td>
-                                <td> {pull.rarity}</td>
-                                <td> <img className = "small_img" src = {pull.img}/></td>
-                            </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    </>}
+                    <p> Loading... </p>
+                </>
+            }
+            {page == "table" && loading == false &&
+                <>
+                    <div className = "grid_container">
+                        {pulls && pulls.map((pull) => (
+                            <div className = "grid_item">
+                                <tbody>
+                                    <tr>{pull.gacha_id}</tr>
+                                    <tr>{pull.rarity}</tr>
+                                    <tr><img className = "small_img" src = {pull.img}/></tr>
+                                </tbody>
+                            </div>))}
+                    </div>
+                    <button className = "buttons_normal" disabled = {disabled}>Next</button>
+                        
+                        
                 </>
             }
             {page == "gamble" && <Gamble uuid = {uuid}/>}
